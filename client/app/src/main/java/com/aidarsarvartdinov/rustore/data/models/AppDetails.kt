17715755d2b258c500968a55c3836d51d571a6ca
AidarSarvartdinov <aidarsarvartdinov2004@gmail.com>
2026-07-14 13:50:24 +0300

    feat(client): add AppSummary, AppDetails models

---
+++ client/app/src/main/java/com/aidarsarvartdinov/rustore/data/models/AppDetails.kt
@@ -0,0 +1,12 @@
+package com.aidarsarvartdinov.rustore.data.models
+
+data class AppDetails(
+    val id: String,
+    val name: String,
+    val description: String,
+    val category: String,
+    val developer: String,
+    val ageRating: String,
+    val iconUrl: String,
+    val screenshots: List<String>
+)
---
+++ client/app/src/main/java/com/aidarsarvartdinov/rustore/data/models/AppSummary.kt
@@ -0,0 +1,9 @@
+package com.aidarsarvartdinov.rustore.data.models
+
+data class AppSummary(
+    val id: String,
+    val name: String,
+    val shortDescription: String,
+    val category: String,
+    val iconUrl: String
+)
